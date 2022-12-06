9cb4632fcbef170a04c5107ef0615c79dd0788b5
Alfonso Niccolo Sales <84696659+AlfonsoSales@users.noreply.github.com>
2022-12-06 20:24:27 +0800

    M3 fix

--- Multiplayer Racing/Assets/Scripts/Shooting.cs
+++ Multiplayer Racing/Assets/Scripts/Shooting.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class Shooting : MonoBehaviour
+public class Shooting : MonoBehaviourPunCallbacks
 {
     [SerializeField]
     Camera fpsCamera;
